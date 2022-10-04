7c697e32e42e09b35552ebfeb3ef99d111fe2360
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-10-04 14:48:49 +0200

    Made pytest. Again global variable caused me a headache

--- src/test_funcs.py
+++ src/test_funcs.py
@@ -11,16 +11,28 @@ from funcs import (
 
 
 def test_prod():
-    assert prod(1, 2, 3) == 'TEST ME'
+    assert prod(1, 2, 3) == 6
+    assert prod(0, 1, 2) == 0
+    assert prod(1, -2, 5) == -10
+
 
 
 def test_prod2():
-    assert prod2(2) == 'TEST ME'
+    #Test should work no matter what global a is in the other file 
+    # I don't know how to set global environment i test file 
+    # In this case it is expected that a=2
+
+    assert prod2(2) == 40
+    assert prod2(0) == 0
+    assert prod2(-1) == -20
 
 
 def test_longest():
-    assert longest([1, 2, 3], [4, 5]) == 'TEST ME'
+    assert longest([1, 2, 3], [4, 5]) == [1, 2, 3]
+    assert longest([1,2], [3,4]) == None
 
 
 def test_dist():
-    assert dist((1, 2), (3, 4)) == 'TEST ME'
+    assert round(dist((1, 2), (3, 4)),2) == 2.83
+    assert round(dist((3,4),(1,2)),2) == 2.83
+    assert dist((0,0), (0,0)) == 0
